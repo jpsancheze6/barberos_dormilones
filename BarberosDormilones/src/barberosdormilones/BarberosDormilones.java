9bd63b7e9a1775026beb946bda41212ab787e9c4
angiemapa <mazariegospao@gmail.com>
2020-04-19 20:06:58 -0500

    fix: estados

--- BarberosDormilones/src/barberosdormilones/BarberosDormilones.java
+++ BarberosDormilones/src/barberosdormilones/BarberosDormilones.java
@@ -11,9 +11,9 @@ public class BarberosDormilones {
         inicio.dormir(1);
         inicio.dormir(2);
         inicio.dormir(3);
-        
         inicio.quitarCliente(1);
         inicio.quitarCliente(2);
         inicio.quitarCliente(3);
+        inicio.actualizarcobro();
     }   
 }
